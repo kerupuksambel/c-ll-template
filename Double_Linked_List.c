c12a4d661cdb1c6e349e0111ed6bc1b70f6d5be8
kerupuksambel <kerupuksambel.2000@gmail.com>
2019-02-25 18:29:16 +0700

    Added function insert_middle()

--- Double_Linked_List.c
+++ Double_Linked_List.c
@@ -46,8 +46,13 @@ void insert_back(int value, linkedlist_t* ll){
 }
 
 void insert_middle(int value, int pos, linkedlist_t* ll){
+    if(pos == 0){
+        insert_front(value, ll);
+        return;
+    }
+
     node_t* box = (node_t*) malloc(sizeof(node_t));
-    node_t* tmp;
+    node_t* tmp = ll->head;
 
 	box->val = value;
 	box->next = NULL;
@@ -56,12 +61,17 @@ void insert_middle(int value, int pos, linkedlist_t* ll){
 	int i;
 	for(i = 0; i < pos; i++){
         tmp = tmp->next;
+
+        if(tmp == NULL){
+            insert_back(value, ll);
+            return;
+        }
 	}
 
-	box->next = tmp->next;
-	box->prev = tmp;
-	tmp->next = box;
-	tmp->next->prev = box;
+    box->next = tmp->next;
+    box->prev = tmp;
+    tmp->next = box;
+    tmp->next->prev = box;
 }
 
 void delete_front(linkedlist_t* ll){
@@ -76,6 +86,22 @@ void delete_back(linkedlist_t* ll){
         ll->tail = ll->tail->prev;
         ll->tail->next = NULL;
     }
+}
+
+void delete_middle(int pos, linkedlist_t* ll){
+    node_t* tmp = ll->head;
+
+    int i;
+    for(i = 0; i < pos; i++){
+        tmp = tmp->next;
+
+        if(tmp == NULL){
+            return;
+        }
+    }
+
+    tmp->next->next->prev = tmp;
+    tmp->next = tmp->next->next;
 }
 
 void print_ll(linkedlist_t* ll){
@@ -94,7 +120,10 @@ int main(){
 	insert_front(25, &list);
 	insert_back(30, &list);
 	insert_back(40, &list);
-	insert_middle(10, 1, &list);
+	insert_middle(10, 2, &list);
+	insert_middle(10, 10, &list);
+	insert_middle(22, 7, &list);
+//	delete_middle(6, &list);
 //	delete_front(&list);
 //	delete_back(&list);
 	print_ll(&list);
